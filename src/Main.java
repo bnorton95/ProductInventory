c635183dd82ac308e1022704ccabad462725f86f
Brian Norton <bnorton95@yahoo.com>
2019-02-27 21:33:34 -0500

    List creation and searching

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
+		/*
 		System.out.println("Enter the product name");
 		String name = scanner.nextLine();
 		
@@ -16,9 +17,34 @@ public class Main {
 		System.out.println("Enter the price: ");
 		double price = scanner.nextDouble();
 		
+
 		Product product1 = new Product(name,id,price,quantity);
-		
 		System.out.println("ID of "+name+" is: "+product1.getID());
+		*/
+		
+		List<Product> list = new ArrayList<Product> (); 
+		Product product2 = new Product("Grapes",1,0.50,30);
+		Product product3 = new Product("Bread",2,0.99,20);
+		Product product4 = new Product("Milk",3,2.50,15);
+		Product product5 = new Product("Chicken",4,5.99,5);
+		
+		list.add(product2);
+		list.add(product3);
+		list.add(product4);
+		list.add(product5);
+		
+		System.out.println("Enter the ID of the product you want to search for: ");
+		int id = scanner.nextInt();
+		
+		for (Product temp : list) {
+			if (temp.getID() == id) {
+				System.out.println("You are looking for "+temp.getName());
+				break;
+			}
+		}
+		
+		
+		
 		
 		
 		scanner.close();
